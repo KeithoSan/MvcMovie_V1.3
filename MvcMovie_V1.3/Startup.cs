839dd9cde9bff7617218eda300f0043c9684151c
Keith <keithsay2@hotmail.com>
2016-02-11 16:25:32 +0000

    Updating Views

---
+++ MvcMovie_V1.3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcMovie_V1._3.Startup))]
+namespace MvcMovie_V1._3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
