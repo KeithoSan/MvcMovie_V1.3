6b20644944007819742c5efdb14d01ac7a4ab543
Keith <keithsay2@hotmail.com>
2016-02-21 20:08:21 +0000

    try to create unit testing

---
+++ MVCMovieTest/MoviesControllerTest.cs
@@ -0,0 +1,20 @@
+﻿//using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using MvcMovie_V1._3.Controllers;
+
+//namespace MVCMovieTest
+//{
+//    [TestClass]
+//    public class MoviesControllerTest
+//    {
+//        [TestMethod]
+//        public void TestDetailsView()
+//        {
+//            MoviesController controller = new MoviesController();
+//            var result = controller.Index as ViewResults; 
+//            Assert.IsNotNull(result);
+
+
+//        }
+//    }
+//}
