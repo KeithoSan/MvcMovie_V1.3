6ced03a0d8085313a7748a9fd6bd3cbbe3f8643f
Keith <keithsay2@hotmail.com>
2016-02-25 17:50:09 +0000

    Unit Test v 1

--- MVCMovieTest/MoviesControllerTest.cs
+++ MVCMovieTest/MoviesControllerTest.cs
@@ -1,20 +1,20 @@
-﻿//using System;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using MvcMovie_V1._3.Controllers;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcMovie_V1._3.Controllers;
 
-//namespace MVCMovieTest
-//{
-//    [TestClass]
-//    public class MoviesControllerTest
-//    {
-//        [TestMethod]
-//        public void TestDetailsView()
-//        {
-//            MoviesController controller = new MoviesController();
-//            var result = controller.Index as ViewResults; 
-//            Assert.IsNotNull(result);
+namespace MVCMovieTest
+{
+    [TestClass]
+    public class MoviesControllerTest
+    {
+        [TestMethod]
+        public void TestDetailsView()
+        {
+            MoviesController controller = new MoviesController();
+            var result = controller.Index as ViewResults;
+            Assert.IsNotNull(result);
 
 
-//        }
-//    }
-//}
+        }
+    }
+}
